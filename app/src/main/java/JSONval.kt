a4c5bcaa792168b3261fc64d4ac46310846902fc
Chris Bailey <cjbailey89@gmail.com>
2022-08-17 06:15:47 +1000

    Hardcode file names

--- app/src/main/java/JSONval.kt
+++ app/src/main/java/JSONval.kt
@@ -2,18 +2,42 @@ import java.io.File
 
 fun main() {
     readFromFile()
+    check()
 }
+
+fun check() {
+    readFromFile()
+}
+
+//Folder of test cases
 var filePath = "T1_test_cases"
+//List each file inside test case folder
+//var fileList: List<String> = listOf("")
 
+val files = listOf(
+    "False_1.json",
+    "False_2.json",
+    "False_3.json",
+    "False_4.json",
+    "False_5.json",
+    "True_1.json",
+    "True_2.json",
+    "True_3.json",
+    "True_4.json")
+
+//takes the file Location and turns the file list in to the file path used to read
+//file into the char array
 fun readFromFile(){
-    fileLocation()
-   var readFromFile: CharArray = File(filePath).readText().toCharArray()
-    print(readFromFile)
+    for (file in files){
+        var readFromFile: CharArray = File("T1_test_cases/$file").readText().toCharArray()
+        print(readFromFile)
 }
-
-fun fileLocation(){
-    File(filePath).walkTopDown().forEach {
-       filePath = (it).toString()
-        println(filePath)
-    }
+//get the file location using the default file path and steps through each file in the fold
+// updating the file path for each test case
+//fun fileLocation(){
+//    File(filePath).walkTopDown().forEach {
+//       filePath = (it).toString()
+//        fileList = listOf(filePath)
+//        print(fileList)
+//    }
 }
